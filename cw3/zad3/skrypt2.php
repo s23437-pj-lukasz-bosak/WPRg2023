4ddc74e067921e4c8aa78b01f2c5eb2565335973
s23437-pj-lukasz-bosak <73651774+s23437-pj-lukasz-bosak@users.noreply.github.com>
2023-04-29 11:16:11 +0200

    Update skrypt2.php

--- cw3/zad3/skrypt2.php
+++ cw3/zad3/skrypt2.php
@@ -37,31 +37,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     fwrite($file, $csvData);
     fclose($file);
 
-
-
-
-//// podsumowanie rezerwacji wypisuje za pomoca echo poszczegolne zmienne.
-//
-//    echo "<h1>PODSUMOWANIE. Zapis do pliku powiódł sie</h1>";
-//    echo "<p><strong>Liczba osób:</strong> $liczba_osob</p>";
-//    echo "<p><strong>Imię:</strong> $imie</p>";
-//    echo "<p><strong>Nazwisko:</strong> $nazwisko</p>";
-//    echo "<p><strong>E-mail:</strong> $email</p>";
-//    echo "<p><strong>Numer telefonu:</strong> $nrtelefonu</p>";
-//    echo "<p><strong>Adres:</strong> $adres</p>";
-//    echo "<p><strong>Numer karty kredytowej:</strong> $kartakredytowa</p>";
-//    echo "<p><strong>Pobyt od: </strong> $dataprzyjazdu</p>";
-//    echo "<p><strong>Pobyt do: </strong> $datawyjazdu</p>";
-//    echo "<p><strong>Łóżko dla dziecka:</strong> $lozkodziecko</p>";
-//
-//// sprawdzam czy  lista z udogodnieniami nie jest pusta, jesli nie wypisuje cala zawartos w petli
-//
-//    if (is_array($udogodnienia) && !empty($udogodnienia)) {
-//        echo "<p><strong>Udogodnienia:</strong> ";
-//        foreach ($udogodnienia as $udogodnienie) {
-//            echo "$udogodnienie, ";
-//        }
-//        echo "</p>";
-//    }
 }
 ?>
